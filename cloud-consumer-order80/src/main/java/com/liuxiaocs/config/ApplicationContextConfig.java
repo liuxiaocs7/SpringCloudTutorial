a2b6ad9eb296a7f6fe46bcecd5f6cd33d3410990
liuxiao <liuxiao2103@qq.com>
2022-06-03 11:02:34 +0800

    update: 集群配置

--- cloud-consumer-order80/src/main/java/com/liuxiaocs/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/liuxiaocs/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.liuxiaocs.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,8 +10,10 @@ public class ApplicationContextConfig {
 
     /**
      * 将restTemplate注入容器
+     * 增加默认的负载均衡机制
      */
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
---
+++ cloud-provider-payment8002/src/main/java/com/liuxiaocs/service/PaymentService.java
@@ -0,0 +1,10 @@
+package com.liuxiaocs.service;
+
+import com.liuxiaocs.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+public interface PaymentService {
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long id);
+}
