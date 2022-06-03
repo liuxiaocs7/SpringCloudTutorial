2c0ac6dce7b3c8ed290392f1a41a864272aa09d4
liuxiao <liuxiao2103@qq.com>
2022-06-03 13:46:27 +0800

    feat: 服务发现

--- cloud-provider-payment8001/src/main/java/com/liuxiaocs/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/liuxiaocs/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.liuxiaocs.entities.Payment;
 import com.liuxiaocs.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -17,6 +20,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
@@ -38,5 +44,25 @@ public class PaymentController {
         } else {
             return new CommonResult(444, "没有对应记录，查询ID: " + id, null);
         }
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+        // 获取微服务名称
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            // *****element: cloud-payment-service
+            // *****element: cloud-order-service
+            log.info("*****element: " + service);
+        }
+
+        // 获取微服务下的主机
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            // CLOUD-PAYMENT-SERVICE	192.168.200.1	8002	http://192.168.200.1:8002
+            // CLOUD-PAYMENT-SERVICE	192.168.200.1	8001	http://192.168.200.1:8001
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
